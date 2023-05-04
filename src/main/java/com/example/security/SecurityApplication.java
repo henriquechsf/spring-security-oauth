f2870872bfcb01ed59ab6043f686aad7cddee6d0
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2023-05-04 17:13:45 -0300

    initial setup

---
+++ src/main/java/com/example/security/SecurityApplication.java
@@ -0,0 +1,23 @@
+package com.example.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class SecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityApplication.class, args);
+	}
+
+}
+
+@RestController
+class HttpController {
+	@GetMapping("/public")
+	String publicRoute() {
+		return "<h1>Public route</h1>";
+	}
+}
